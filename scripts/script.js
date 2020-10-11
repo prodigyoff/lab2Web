1e604b3d6f941d03ca47b3bafd5105a5c7e691d2
prodigyoff <maryan.kit@gmail.com>
2020-10-11 16:03:22 +0300

    Added lab2 files

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const menuButton = document.querySelector('.menu-button');
+const navLinks = document.querySelector('.top-nav');
+let menuOpen = false;
+menuButton.addEventListener('click', () =>{
+    if(!menuOpen){
+        menuButton.classList.add('open');
+        navLinks.classList.add('show');
+        menuOpen = true;
+    } else {
+        menuButton.classList.remove('open');
+        navLinks.classList.remove('show');
+        menuOpen = false;
+    }
+});
